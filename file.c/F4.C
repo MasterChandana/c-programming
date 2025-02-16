3a9803a95cac4d64aedee2ccc9696e62514b7bda
M. CHANDANA KUMAR PATRA <cp987154@gmail.com>
2025-02-16 20:39:47 +0530

    Add files via upload

---
+++ file.c/F4.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    FILE *file = fopen("output.txt", "w");
+
+    if (file == NULL)
+    {
+        printf("Error!\n");
+        return 1;
+    }
+
+    fprintf(file, "Hello, this is a test.\n");
+    printf("Data written successfully.\n");
+
+    fclose(file);
+    return 0;
+}
